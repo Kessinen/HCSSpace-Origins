067635d91353c95288b150d0dc6ff6dc56395230
Esa Kataja <ekataja@gmail.com>
2021-05-10 17:33:57 +0300

    ADD Enemy level variable

--- src/Enemies/enemies.gd
+++ src/Enemies/enemies.gd
@@ -1,9 +1,12 @@
 extends KinematicBody2D
 
-export var HP : int = 10
+export var HPBase : int = 3
 export var Shields : int = 0
 export var Loot : int = 0
-export var Speed : int = 0
+export var Speed : int = 30
+export var enemyLevel : int = 1
+
+var HP = HPBase * enemyLevel
 
 func takeDamage(amount):
 	if amount > Shields:
