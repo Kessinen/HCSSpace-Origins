d880ddb8cb09797dca52f9ae5774c622ac92cf2b
Esa Kataja <ekataja@gmail.com>
2021-05-10 16:33:31 +0300

    ADD Bullet collision detection

--- src/Bullets/Player/Bullet1.gd
+++ src/Bullets/Player/Bullet1.gd
@@ -8,3 +8,8 @@ func _physics_process(delta):
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+
+func _on_Bullet1_body_entered(body):
+	if body.is_in_group("Enemies"):
+		body.takeDamage(bulletDamage)
+		queue_free()
