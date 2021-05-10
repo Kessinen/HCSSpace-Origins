c2c737de70e1a227ecca593f3a445f04e271ab15
Esa Kataja <ekataja@gmail.com>
2021-05-10 16:31:46 +0300

    ADD Enemies ability to take damage

--- src/Enemies/enemies.gd
+++ src/Enemies/enemies.gd
@@ -4,3 +4,12 @@ export var HP : int = 10
 export var Shields : int = 0
 export var Loot : int = 0
 export var Speed : int = 0
+
+func takeDamage(amount):
+	HP -= amount
+	if HP <= 0:
+		die()
+	print("Damage taken ", amount, " HP left ", HP)
+
+func die():
+	queue_free()
