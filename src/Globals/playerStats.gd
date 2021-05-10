b8ac38db90c09761b33b0a9d6a3bc7286ca4e726
Esa Kataja <ekataja@gmail.com>
2021-05-10 21:12:39 +0300

    ADD Player stats singleton

---
+++ src/Globals/playerStats.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var lootAmount : int
+var shipHP : int = 10
+var shipShields : int = 0
+var shipRoF : int = 1
+var shipDamage : int = 10
+var shipHandling : int = 10
+var shipMagnet : int = 10
+var shipSpeed : int = 10
