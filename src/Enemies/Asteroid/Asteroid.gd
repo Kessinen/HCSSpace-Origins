135c9df5f4a6a4cc6c81e63dc41cfdef97122c8c
Esa Kataja <ekataja@gmail.com>
2021-05-10 17:36:28 +0300

    CHANGE Rotate asteroid sprite instead of entity

--- src/Enemies/Asteroid/Asteroid.gd
+++ src/Enemies/Asteroid/Asteroid.gd
@@ -3,10 +3,28 @@ extends "res://Enemies/enemies.gd"
 export var RotationSpeed : int = 2
 var velocity : Vector2 = Vector2.DOWN
 
+var plAsteroid = preload("res://Enemies/Asteroid/miniAsteroid.tscn")
+
 func _ready():
 	randomize()
-	rotation_degrees = rand_range(0,360)
+	$sprite.rotation_degrees = rand_range(0,360)
 
 func _physics_process(delta):
-	rotate((float(RotationSpeed) / 10) * delta)
+	
+	$sprite.rotate((float(RotationSpeed) / 10) * delta)
 	position.y = position.y + Speed * delta
+	
+	
+
+func die():
+
+	for i in range(enemyLevel * 2):
+		spawnChild()
+	queue_free()
+
+func spawnChild():
+	var enemy = plAsteroid.instance()
+	enemy.position = global_position
+	
+	get_parent().add_child(enemy)
+	pass
