0564e38534950abcc6864ceb64ba9f36c7fe11f1
Esa Kataja <ekataja@gmail.com>
2021-05-10 14:04:45 +0300

    ADD Asteroid physics & collision

--- src/Enemies/Asteroid/Asteroid.gd
+++ src/Enemies/Asteroid/Asteroid.gd
@@ -1,2 +1,9 @@
 extends "res://Enemies/enemies.gd"
 
+export var RotationSpeed : int = 2
+var velocity : Vector2 = Vector2.DOWN
+
+func _physics_process(delta):
+	rotate((float(RotationSpeed) / 10) * delta)
+	position.y = position.y + Speed * delta
+	pass
