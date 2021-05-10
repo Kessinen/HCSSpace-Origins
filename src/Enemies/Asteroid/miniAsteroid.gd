634494351f97d33988801144e2a763f1a73a7fc0
Esa Kataja <ekataja@gmail.com>
2021-05-10 20:11:24 +0300

    ADD Miniasteroids drop loot

--- src/Enemies/Asteroid/miniAsteroid.gd
+++ src/Enemies/Asteroid/miniAsteroid.gd
@@ -1,5 +1,7 @@
 extends "res://Enemies/enemies.gd"
 
+var plLoot = preload("res://Loot/Loot.tscn")
+
 func _ready():
 	randomize()
 	rotate(deg2rad(rand_range(0,90) - 45))
@@ -10,3 +12,11 @@ func _physics_process(delta):
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+
+func _exit_tree():
+	var loot = plLoot.instance()
+	loot.lootValue = enemyLevel * 10
+	loot.position = global_position
+	get_parent().add_child(loot)
+	
+
