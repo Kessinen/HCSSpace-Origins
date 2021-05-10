f268d22361a867e422576219e2185d9437de399e
Esa Kataja <ekataja@gmail.com>
2021-05-10 14:46:52 +0300

    ADD Asteroid rotation randomize

--- src/Enemies/Asteroid/Asteroid.gd
+++ src/Enemies/Asteroid/Asteroid.gd
@@ -3,7 +3,10 @@ extends "res://Enemies/enemies.gd"
 export var RotationSpeed : int = 2
 var velocity : Vector2 = Vector2.DOWN
 
+func _ready():
+	randomize()
+	rotation_degrees = rand_range(0,360)
+
 func _physics_process(delta):
 	rotate((float(RotationSpeed) / 10) * delta)
 	position.y = position.y + Speed * delta
-	pass
