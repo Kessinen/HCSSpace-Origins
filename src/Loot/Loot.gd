4de5f5830e89bf7bfc59f9caa81308d46c76dc3a
Esa Kataja <ekataja@gmail.com>
2021-05-10 21:15:01 +0300

    ADD Loot attract

--- src/Loot/Loot.gd
+++ src/Loot/Loot.gd
@@ -1,7 +1,25 @@
-extends Sprite
+extends KinematicBody2D
+
+onready var playerStats = get_node("/root/playerStats")
 
 export var lootSpeed : int = 20
 export var lootValue : int = 1
 
+var attractTarget
+
+func _ready():
+	$Magnet/CollisionShape2D.shape.radius = playerStats.shipMagnet * 20
+
 func _physics_process(delta):
-	position.y += lootSpeed * delta
+	if attractTarget != null:
+		moveTowardsPlayer()
+	else:
+		position.y += lootSpeed * delta
+
+func moveTowardsPlayer():
+	var moveDirection = attractTarget.global_position - global_position
+	move_and_slide(moveDirection)
+
+func _on_Magnet_body_entered(body):
+	if body.name == "Player":
+		attractTarget = body
