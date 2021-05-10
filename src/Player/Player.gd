4674719f9f453ceae77383f29aa1035b0ff889bc
Esa Kataja <ekataja@gmail.com>
2021-05-10 19:38:35 +0300

    ADD Player ship stats

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -3,41 +3,48 @@ extends KinematicBody2D
 export var Name : String = "Player1"
 export var HP: int = 10
 export var Shield: int = 0
-export var MaxSpeed : int = 2000
-export var MagnetRadius : int = 10
-export var Acceleration : int = 10
 
 var Score : int = 0
+
+var shipRoF : int = 1
+var shipDamage : int = 1
+var shipHandling : int = 1
+var shipMagnet : int = 1
+var shipSpeed : int = 1
+
 
 var velocity = Vector2.ZERO
 
 onready var plBullet1 = preload("res://Bullets/Player/Bullet1.tscn")
 
-func _ready():
-	pass # Replace with function body.
-
 func _physics_process(delta):
+	
+	moveShip()
+	
+	if Input.is_action_pressed("shoot"):
+		fireGuns()
+
+func moveShip():
 	var input_vector : Vector2
 	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	input_vector = input_vector.normalized()
-	
 	if input_vector != Vector2.ZERO:
-		velocity = velocity.move_toward(input_vector * MaxSpeed, Acceleration)
-		
+		velocity = velocity.move_toward(input_vector * shipSpeed*100, shipHandling)
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, Acceleration)
-		
+		velocity = velocity.move_toward(Vector2.ZERO, shipHandling)
 	velocity = move_and_slide(velocity)
-	
-func _unhandled_key_input(event):
-	if event.is_action_pressed("shoot"):
-		fireGuns()
 
 func fireGuns():
+	var rofTimer = $rofTimer
+	rofTimer.wait_time = 1 - (float(shipRoF) * 0.099)
 	var bullet = plBullet1.instance()
+	bullet.bulletDamage = shipDamage
 	bullet.position = $Gun.global_position
-	get_parent().add_child(bullet)
+
+	if rofTimer.is_stopped():
+		get_parent().add_child(bullet)
+		rofTimer.start()
 	
 
 func takeDamage(amount : int):
