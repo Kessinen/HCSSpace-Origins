0cc84c28f30196ed7496af7fe00a45f7c2f11781
Esa Kataja <ekataja@gmail.com>
2021-05-13 19:08:08 +0300

    ADD Levels ending conditions

--- src/Levels/levels.gd
+++ src/Levels/levels.gd
@@ -1,7 +1,14 @@
 extends StaticBody2D
 
-var PlayerStartingPos : Vector2
+onready var PlayerStartingPos := Vector2(get_viewport().size.x / 2,get_viewport().size.y - 64)
+onready var playerStats = get_node("/root/playerStats")
 
-func _ready():
-	PlayerStartingPos = Vector2(get_viewport().size.x / 2,get_viewport().size.y - 64)
+func levelComplete(level : int):
+	get_tree().change_scene("res://Menus/LevelSelect.tscn")
+	if playerStats.highestLevel <= level:
+		playerStats.highestLevel = level+1
+	pass
 
+func levelDefeat():
+	get_tree().change_scene("res://Menus/LevelSelect.tscn")
+	pass
