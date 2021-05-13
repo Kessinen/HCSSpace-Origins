17dcb9b565abebd1490a69a4fa0db24dc0b667ad
Esa Kataja <ekataja@gmail.com>
2021-05-13 19:08:30 +0300

    ADD Level 1 ending

--- src/Levels/Lvl1/Level1.gd
+++ src/Levels/Lvl1/Level1.gd
@@ -1,10 +1,17 @@
 extends "res://Levels/levels.gd"
 
-
 func _ready():
 	var player = $Player
 	player.position = PlayerStartingPos
 	player.z_index = -1
-	self.add_child(player)
 	
 	$EnemySpawner.spawnEnemy("Asteroid",10, Vector2(0,500))
+
+func _process(delta):
+	checkLevelStatus()
+
+func checkLevelStatus():
+	var enemies := get_tree().get_nodes_in_group("Enemies").size()
+	var lootOnScreen := get_tree().get_nodes_in_group("Loot").size()
+	if enemies + lootOnScreen == 0:
+		levelComplete(1)
