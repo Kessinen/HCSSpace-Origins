0eb18715b3689265c6f4d5938bcb7f0991687e2a
Esa Kataja <ekataja@gmail.com>
2021-05-13 23:39:13 +0300

    CHANGE Builtools config version
    Save program version to project.godot file

--- build.py
+++ build.py
@@ -15,23 +15,47 @@ def getLastestHash() -> str:
     gitHash = run(cmd, capture_output=True).stdout.decode("utf-8")
     return gitHash.strip()
 
+def writeExport(confFile):
+    config = ConfigParser()
+    config.read(confFile)
+    config['preset.0']['export_path'] = f'"../bin/HCSpaceOrigins-linux-{getLatestTag()}-{getLastestHash()}.x86_64"'
+    config['preset.1']['export_path'] = f'"../bin/HCSpaceOrigins-win64-{getLatestTag()}-{getLastestHash()}.exe"'
+    config['preset.1.options']['application/product_version'] = f'"{getLatestTag()}_{getLastestHash()}"'
+
+    with open(confFile, 'w') as iniFile:
+        config.write(iniFile)
+
+def writeProjectVersion(file):
+    with open(file, 'r') as proFile:
+        proFileLines = proFile.readlines()
+    
+    newFileContents = ""
+    for line in proFileLines:
+        if line.strip()[:15] == "config/version=":
+            line = f'config/version="{getLatestTag()}"'
+        
+        newFileContents += line
+    
+    with open(file, 'w') as newproFile:
+        newproFile.write(newFileContents)
+
+
+def generateBinaries():
+    exports = ["linux", "win64"]
+
+    for name in exports:
+        cmd = f'godot --path src --export {name}'.split()
+        run(cmd)
+
+
 #Check if export file exists
-configFile = Path("src/export_presets.cfg")
-if not configFile.exists():
+exportFile = Path("src/export_presets.cfg")
+projectFile = Path("src/project.godot")
+
+if not exportFile.exists() or not projectFile.exists():
     print("ERROR: Export config file not found!")
     exit(1)
 
-config = ConfigParser()
-config.read("src/export_presets.cfg")
-config['preset.0']['export_path'] = f'"../bin/HCSpaceOrigins-linux-{getLatestTag()}-{getLastestHash()}.x86_64"'
-config['preset.1']['export_path'] = f'"../bin/HCSpaceOrigins-win64-{getLatestTag()}-{getLastestHash()}.exe"'
-config['preset.1.options']['application/product_version'] = f'"{getLatestTag()}_{getLastestHash()}"'
-
-with open(configFile, 'w') as iniFile:
-    config.write(iniFile)
-
-exports = ["linux", "win64"]
-
-for name in exports:
-    cmd = f'godot --path src --export {name}'.split()
-    run(cmd)
+writeExport(exportFile)
+writeProjectVersion(projectFile)
+#generateBinaries()
