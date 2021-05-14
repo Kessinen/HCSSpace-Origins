b3c28cabf3caca36347ae7f7d3676b6d31b07f2e
Esa Kataja <ekataja@gmail.com>
2021-05-14 04:27:22 +0300

    FIX Asteroid rotation
    Fixes issue #1

--- src/Enemies/Asteroid/Asteroid.gd
+++ src/Enemies/Asteroid/Asteroid.gd
@@ -2,15 +2,18 @@ extends "res://Enemies/enemies.gd"
 
 export var RotationSpeed : int = 2
 var velocity : Vector2 = Vector2.DOWN
-
+var rotateDirection : int
 var plAsteroid = preload("res://Enemies/Asteroid/miniAsteroid.tscn")
 
 func _ready():
 	randomize()
 	$sprite.rotation_degrees = rand_range(0,360)
+	rotateDirection = round(rand_range(0,1))
+	if rotateDirection == 0:
+		rotateDirection = -1	
 
 func _physics_process(delta):
-	$sprite.rotate((float(RotationSpeed) / 10) * delta)
+	$sprite.rotate((float(RotationSpeed) / 10) * delta * rotateDirection)
 	position.y = position.y + Speed * delta
 
 func die():
--- src/Enemies/Asteroid/miniAsteroid.gd
+++ src/Enemies/Asteroid/miniAsteroid.gd
@@ -1,11 +1,20 @@
 extends "res://Enemies/enemies.gd"
 
+export var RotationSpeed : int = 2
+var rotateDirection : int
+
 func _ready():
 	randomize()
 	rotate(deg2rad(rand_range(0,90) - 45))
+	$sprite.rotate(deg2rad(rand_range(0,360)))
+	rotateDirection = round(rand_range(0,1))
+	if rotateDirection == 0:
+		rotateDirection = -1
 	
 func _physics_process(delta):
 	move_and_slide(Vector2.DOWN.rotated(rotation) * Speed)
+	
+	$sprite.rotate((float(RotationSpeed) / 10) * delta * rotateDirection)
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
