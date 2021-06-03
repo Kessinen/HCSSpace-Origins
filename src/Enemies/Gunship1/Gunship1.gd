100d9c7b87fffeb2f49125bdf004f523b76b6f6b
Esa Kataja <ekataja@gmail.com>
2021-06-03 17:57:07 +0300

    ADD New enemy Gunship

---
+++ src/Enemies/Gunship1/Gunship1.gd
@@ -0,0 +1,32 @@
+extends "res://Enemies/enemies.gd"
+
+var rofTimer = Timer.new()
+
+export (PackedScene) var psBullet
+
+func _ready():
+	rofTimer.wait_time = RoF
+	rofTimer.one_shot = true
+	add_child(rofTimer)
+
+func _physics_process(delta):
+	move(delta)
+
+func _process(delta):
+	shoot()
+
+func move(delta) -> void:
+	var collision
+	collision = move_and_collide(Vector2.DOWN * Speed * delta)
+
+func shoot():
+	if rofTimer.is_stopped():
+		for gun in $Guns.get_children():
+			var bullet = psBullet.instance()
+			bullet.position = gun.global_position
+			bullet.rotation = deg2rad(180)
+			get_parent().add_child(bullet)
+		rofTimer.start()
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
