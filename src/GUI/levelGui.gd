35a3d8ce180bdcf6316547bf7e274caae2e9a062
Esa Kataja <ekataja@gmail.com>
2021-06-04 13:42:12 +0300

    ADD Connected HP progressbar signals

--- src/GUI/levelGui.gd
+++ src/GUI/levelGui.gd
@@ -1,4 +1,16 @@
 extends Control
 
+onready var playerHP = int(get_node("/root/playerStats").playerData["shipHP"])
+onready var playerMaxHP = int(get_node("/root/playerStats").upgradeMaxValues["HP"])
+
+func _ready():
+	$CanvasLayer/MarginContainer/GridContainer/TextureProgress.max_value = int(range_lerp(playerHP,1,10,1,playerMaxHP))
+	$CanvasLayer/MarginContainer/GridContainer/TextureProgress.value = int(range_lerp(playerHP,1,10,1,playerMaxHP))
+	
+
 func _on_Player_lootChanged(newValue):
 	$CanvasLayer/MarginContainer/GridContainer/TextureRect3/lootAmount.text = String(newValue)
+
+
+func _on_Player_hpChanged(newValue):
+	$CanvasLayer/MarginContainer/GridContainer/TextureProgress.value = newValue
